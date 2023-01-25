f8b92fd6b7586034efa272ef98027fe4946eb12c
Alex McKenzie <alexmckenzie1990@gmail.com>
2023-01-25 12:40:23 +0000

    Created myLeads array and inputEl

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let inputButton = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputButton = document.getElementById("input-btn")
 
 inputButton.addEventListener("click", function() {
   console.log("Button clicked from event listener!")
