afa7d73a3e999357e17fb9f3294a7ddc10fcd1f5
Alex McKenzie <alexmckenzie1990@gmail.com>
2023-01-26 11:22:53 +0000

    Refactored to use template strings

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const inputEl = document.getElementById("input-el")
 const inputButton = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-
 inputButton.addEventListener("click", function() {
   myLeads.push(inputEl.value)
   inputEl.value = ""
@@ -13,7 +12,13 @@ inputButton.addEventListener("click", function() {
 function renderLeads() {
   let listItems = ""
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
+    listItems += `
+      <li>
+        <a target="_blank" href="${myLeads[i]}">
+          ${myLeads[i]}
+        </a>
+      </li>
+    `
   }
   ulEl.innerHTML = listItems
 }
