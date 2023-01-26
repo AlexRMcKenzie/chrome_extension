53df80fd3055a40a40eaab99268f1f266e38c6e4
Alex McKenzie <alexmckenzie1990@gmail.com>
2023-01-26 10:10:37 +0000

    Added innerHTML

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputButton = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+
 
 inputButton.addEventListener("click", function() {
   myLeads.push(inputEl.value)
   console.log(myLeads)
 })
 
+let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
-  console.log(myLeads[i])
+  listItems += "<li>" + myLeads[i] + "</li>"
+  console.log(listItems)
 }
+
+ulEl.innerHTML = listItems
